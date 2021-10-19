fdb24e89a12c530633b7223c7701899a294e4b2c
Lucas Almeida <developer.lucas24@gmail.com>
2021-10-19 11:43:50 +0100

    chore: add bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/styles.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
