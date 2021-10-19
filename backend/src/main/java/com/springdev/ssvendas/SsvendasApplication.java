0cfa26971d3d749c5834fa1fca5598b53a68458b
Lucas Almeida <developer.lucas24@gmail.com>
2021-10-19 10:47:17 +0100

    chore: project created

---
+++ backend/src/main/java/com/springdev/ssvendas/SsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.springdev.ssvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/springdev/ssvendas/SsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdev.ssvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
