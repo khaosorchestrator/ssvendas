122f4f7d29ae181d89abf8383b38b76889dd747c
Lucas Almeida <developer.lucas24@gmail.com>
2021-10-20 15:13:03 +0100

    feat: domain model and database seed

---
+++ backend/src/main/java/com/springdev/ssvendas/domain/Sale.java
@@ -0,0 +1,30 @@
+package com.springdev.ssvendas.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+}
