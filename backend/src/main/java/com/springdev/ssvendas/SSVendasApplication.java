b7ddcd36df36db76415506f33faee12db912f700
Lucas Almeida <developer.lucas24@gmail.com>
2021-10-20 14:32:51 +0100

    feat: security config

--- backend/src/main/java/com/springdev/ssvendas/SSVendasApplication.java
+++ backend/src/main/java/com/springdev/ssvendas/SSVendasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsvendasApplication {
+public class SSVendasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsvendasApplication.class, args);
+		SpringApplication.run(SSVendasApplication.class, args);
 	}
 
 }
